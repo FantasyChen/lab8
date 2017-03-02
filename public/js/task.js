f2df2b8b32a5f216ac4a39cf9067daa1ce6ad4a3
FantasyChen <524413916@qq.com>
2017-03-02 11:46:01 -0800

    add partials

---
+++ public/js/task.js
@@ -0,0 +1,16 @@
+// Your code goes here
+$(document).ready(function() {
+$('#taskForm').submit(function(e) {
+    // Prevents default submit + reload (we only want the submit part)
+    e.preventDefault();
+    var taskName = $('#addTask').val();
+    // Send the POST request
+    $.post('/addTask', { taskName: taskName }, postCallback);
+    return false;
+  });
+});
+
+function postCallback(res) {
+  $('.taskList').append('<li>' + res + '</li>'); // Add to the list
+ $('#addTask').val(''); // Clear form
+}
